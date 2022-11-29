ea38648621509708330b79ca76ae89ac605ef0f5
kjw <yoonlight12@gmail.com>
2022-11-29 21:57:32 +0900

    chore: first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Frontend
+
+## Dependency
+
+| name         | version |
+| ------------ | ------- |
+| React        | 18.2.0  |
+| Node         | 18.2.0  |
+| React Router | 6.4.3   |
+| React Query  | 3.39.2  |
+| typescript   | 4.6.4   |
+| Fontawesome  | 6.2.1   |
+| Fullcalendar | 5.11.3  |
---
+++ src/main.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
+import { router } from "./router";
+import "css-wipe";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { store } from "./store";
+import { Provider } from "react-redux";
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </Provider>
+  </React.StrictMode>
+);
