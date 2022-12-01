bf3f06b906bcad3d58017a60422fea7b86e87696
kjw <yoonlight12@gmail.com>
2022-12-01 23:42:57 +0900

    feat(api): add auth (#5)

---
+++ src/api/auth.interface.ts
@@ -0,0 +1,14 @@
+export interface IRegsiterRequest {
+  uid: string;
+  name?: string;
+  authProvider: string;
+  email?: string;
+}
+
+export interface ILoginRequest {
+  token: string;
+}
+
+export interface ILoginResponse {
+  accessToken: string;
+}
